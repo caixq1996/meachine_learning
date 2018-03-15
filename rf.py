d367099427a70ffb4a2dd384647f0a46dae44d86
jkrs <584533658@qq.com>
2018-03-15 21:36:40 +0800

    Update and rename rf_temp.py to rf.py

--- rf.py
+++ rf.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar  7 18:54:11 2018
+Created on Thu Mar 15 20:50:11 2018
 
-@author: jkrs
+@author: 蔡欣强
 """
 
 from math import log
+from math import ceil
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 import operator
+import os
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
@@ -26,27 +29,18 @@ def ent(data):
         s -= p * log(p,2)
     return s
 
-def remove_feature(data,i,split_point,value,flag):
+def remove_feature(data,i,value,flag):
     newdata = []
-#    print('split_point = ',split_point)
-    num = 0
-    n = len(data[0]) - 1
-    for j in range(len(data)):
+    for row in data:
         if flag == True:
-            if num == split_point:
-                break
-            if data[j][i] <= value:
-                num += 1
-                temp = data[j][:i]
-                temp.extend(data[j][i + 1:])
+            if row[i] < value:
+                temp = row[:i]
+                temp.extend(row[i + 1:])
                 newdata.append(temp)
         else:
-            if num == n - split_point:
-                break
-            if data[j][i] >= value:
-                num += 1
-                temp = data[j][:i]
-                temp.extend(data[j][i + 1:])
+            if row[i] >= value:
+                temp = row[:i]
+                temp.extend(row[i + 1:])
                 newdata.append(temp)
 #    print('newdata = ',newdata)
     return newdata
@@ -58,15 +52,15 @@ def choosebest(data):
     bestpoint = -1.0
     n = len(data[0]) - 1
     S = ent(data)
-    split_num = -2
     for i in range(n):
         curfeature = []
         for j in range(m):
             curfeature.append(data[j][i])
-        curfeature.sort()
+        curfeature = set(curfeature)
+        curfeature = list(curfeature)
         maxgain = 0.0
         point_id = -1
-        for j in range(m - 1):
+        for j in range(len(curfeature) - 1):
             point = float(curfeature[j + 1] + curfeature[j]) / 2
             p1 = float(j + 1) / m
             p2 = float(m - j - 1) / m
@@ -77,13 +71,12 @@ def choosebest(data):
                 split -= p2 * log(p2,2)
             if split == 0:
                 continue
-            gain = (S - p1 * ent(remove_feature(data,i,j + 1,point,True)) - p2 * ent(remove_feature(data,i,j + 1,point,False))) / split
+            gain = (S - p1 * ent(remove_feature(data,i,point,True)) - p2 * ent(remove_feature(data,i,point,False))) / split
             if gain > maxgain:
                 maxgain = gain
                 bestfeature = i
                 bestpoint = point
-                split_num = j
-    return bestfeature,split_num + 1,bestpoint
+    return bestfeature,bestpoint
         
         
 
@@ -114,15 +107,15 @@ def build(data,feature):
         return curlabel[0]
     if  len(data[0]) == 1:
         return majorityCnt(curlabel)
-    i,j,point = choosebest(data)
+    i,point = choosebest(data)
 #    print('i = ',i,'j = ',j)
     bestfeature = feature[i]
     tree = {bestfeature : {}}
     del feature[i]
     newfeature = feature[:]
-    newdata = remove_feature(data,i,j,point,True)
+    newdata = remove_feature(data,i,point,True)
     tree[bestfeature][0] = build(newdata,newfeature)
-    newdata = remove_feature(data,i,j,point,False)
+    newdata = remove_feature(data,i,point,False)
     newfeature = feature[:]
     tree[bestfeature][point] = build(newdata,newfeature)
     return tree
@@ -137,40 +130,49 @@ def dfs(tree,deep,sample):
         cnt = max(cnt,dfs(tree[key],deep + 1,sample))
     return cnt
 
-def main():
-    minn = 100
-    maxn = -1
-    iris = load_iris()
-    x = iris['data']
-    y = iris['target']
-    feature = iris['feature_names']
-    result = []
-    for step in range(1):
-        data, test_data, label, ans = train_test_split(x,y, test_size = 0.3)
-        data = data.tolist()
-        label = label.tolist()
-        num = len(data)
-        for i in range(num):
-            data[i].append(label[i])
-    #    print('data = ',data)
-        test_feature = feature[:]
-        tree = build(data,test_feature)
-        test_data = test_data.tolist()
-        num = len(test_data)
-        res = []
-        for i in range(num):
-            res.append(classify(tree,feature,test_data[i]))
-        cnt = 0
-        ans = ans.tolist()
-        for i in range(num):
-            if ans[i] == res[i]:
-                cnt += 1
-    #    print('precise = ',cnt * 1.0 / num)
-        result.append(cnt * 1.0 / num)
-        maxn = max(maxn,cnt * 1.0 / num)
-        minn = min(minn,cnt * 1.0 / num)
-    print('maxn = ',maxn,'minn = ',minn)
-    print(result)
+def get_subsample(data,ratio,label):
+    data = np.array(data)
+    Index = random.sample(list(range(len(data))),ceil(len(data) * ratio))
+    subdata = data[Index]
+    subdata = subdata.tolist()
+    for i in range(len(Index)):
+        subdata[i].append(label[Index[i]])
+    return subdata
 
-if __name__ == '__main__':
-    main()
+iris = load_iris()
+x = iris['data']
+y = iris['target']
+feature = iris['feature_names']
+data, test_data, label, ans = train_test_split(x,y, test_size = 0.3)
+num = len(data)
+rf = []
+for i in range(5):
+    subdata = get_subsample(data,0.7,label)
+    print('subdata = ',subdata)
+    test_feature = feature[:]
+    rf.append(build(subdata,test_feature))
+#for tree in rf:
+#    print('tree = ',tree)
+num = len(test_data)
+res = []
+for i in range(num):
+    most_cnt = {}
+    maxcnt = 0
+    maxid = -1
+    for tree in rf:
+        Index = classify(tree,feature,test_data[i])
+        if Index not in most_cnt:
+            most_cnt[Index] = 1
+        else:
+            most_cnt[Index] += 1
+        if most_cnt[Index] > maxcnt:
+            maxcnt = most_cnt[Index]
+            maxid = Index
+#    print('most = ',most_cnt)
+#    print('label = ',ans[i])
+    res.append(Index)
+cnt = 0
+for i in range(num):
+    if ans[i] == res[i]:
+        cnt += 1
+print('precise = ',cnt * 1.0 / num)
